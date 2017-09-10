254f0d2e2892824e60b91a5cba1c8a03548fd131
pacopicon <palmtreerooskee@gmail.com>
2017-09-10 16:35:19 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Playground is for testing APIs that require live pages.  If you have any questions, issues, concerns and/or suggestions, please e-mail paco at palmtreerooskee@gmail.com
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Playground</h2>
+        </div>
+        <p className="App-intro">
+          Playground is for testing APIs that require live pages.  If you have any questions, issues, concerns and/or suggestions, please e-mail <code>paco</code> at <code>palmtreerooskee@gmail.com</code>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
